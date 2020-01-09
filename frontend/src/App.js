d6b6d5d917be8b941a0970822da750b81efb77d6
Daniel Mantler <daniel-mantler@live.de>
2020-01-09 10:11:23 +0100

    added gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import DropDown from './components/DropDown'
-import thmlogo from './assets/thm_logo.png'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import DropDown from "./components/DropDown";
+import thmlogo from "./assets/thm_logo.png";
 
 function App() {
   return (
     <div className="App">
-
-<div class="dropDowns">
-  <h1>Project for "Knowledgebased Methods"</h1>
-  <DropDown/>
-    </div>
-    <div class="infoleft">
-    <a href="https://github.com/dmntlr/pocknowledgewebapp">Github</a>
-    </div>
-    <div class="inforight">
-    <img src={thmlogo} width="200px"/>
-    </div>
+      <div class="dropDowns">
+        <h1>Project for "Knowledgebased Methods"</h1>
+        <DropDown />
+      </div>
+      <div class="infoleft">
+        <a href="https://github.com/dmntlr/pocknowledgewebapp">Github</a>
+      </div>
+      <div class="inforight">
+        <img src={thmlogo} width="200px" />
+      </div>
     </div>
   );
 }
